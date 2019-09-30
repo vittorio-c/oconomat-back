ca15aa91f3f77335a92fc3f8eb468625c74686fb
vittorio-c <chillemi.vittorio@gmail.com>
2019-09-30 13:53:58 +0200

    find menu and user as json

--- oconomat/src/Controller/MenuController.php
+++ oconomat/src/Controller/MenuController.php
@@ -4,7 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Menu;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 
 /**
@@ -24,10 +30,23 @@ class MenuController extends AbstractController
      */
     public function find(Menu $menu)
     {
-        return $this->json([
-            'message' => 'hello MenuController->find()',
-            'menu' => $menu,
+        $encoder = [new JsonEncoder()];
+        $normalizers = array(new DateTimeNormalizer(), new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoder);
+
+        // normalize with the data we want
+        $data = $serializer->normalize($menu, null, [
+            'attributes' => [
+                'id', 'createdAt', 'updatedAt',
+                'user' => ['id'],
+                'recipes' => ['id']
+            ]
         ]);
+
+        // encode in json
+        $data = $serializer->encode($data, 'json');
+
+        return new Response($data, 200, ['Content-Type' => 'application/json']);
     }
 
     /**
