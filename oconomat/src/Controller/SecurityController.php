a0d15faa53e8ba8ad9cf2bb69609b63f8a6030e5
Joffreycormont <j.cormont30@gmail.com>
2019-10-02 15:59:37 +0200

    fin résolution du conflit + ajout de l'authentification pour le login avec jwtBundle (token)

--- oconomat/src/Controller/SecurityController.php
+++ oconomat/src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils, Security $security, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         // if ($this->getUser()) {
         //    $this->redirectToRoute('target_path');
