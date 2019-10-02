b9a066a05483b5b6bd73e6d87d05d12b76284ee1
Joffreycormont <j.cormont30@gmail.com>
2019-10-02 15:55:43 +0200

    remove one use statement

--- oconomat/src/Controller/SecurityController.php
+++ oconomat/src/Controller/SecurityController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
