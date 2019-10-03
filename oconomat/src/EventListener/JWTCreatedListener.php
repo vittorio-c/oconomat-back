431e2664e745af863cae9eca7eb737b7519e1dc1
Joffreycormont <j.cormont30@gmail.com>
2019-10-03 15:18:48 +0200

    adding user id in the JWT response

---
+++ oconomat/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+
+class JWTCreatedListener
+{
+
+    public function onJwtCreated(JWTCreatedEvent $event): void
+    {
+        $user = $event->getUser();
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+
+        //$event->setData($payload);
+
+        $event->setData([
+            'payload' => $event->getData(),
+        ]);
+    }
+
+}
