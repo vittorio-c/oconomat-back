7c224aae9ab43ec2277901cd3a6c1b4f56e110fe
vittorio-c <chillemi.vittorio@gmail.com>
2019-10-03 13:01:35 +0200

    basic menu generation

--- oconomat/src/Controller/ObjectifController.php
+++ oconomat/src/Controller/ObjectifController.php
@@ -38,104 +38,29 @@ class ObjectifController extends AbstractController
     public function generateMenu(Request $request)
     {
         $data = json_decode($request->getContent(), true);
+
         $budget = $data['budget'];
         $quantity = 21;
+        // prix objectif par recette
         $targetPrice = $budget / $quantity;
 
-        $em = $this->getDoctrine()->getRepository(Recipe::class);
-        $recipes = $em->findAll();
+        $recipes = $this->getAllRecipesWithPrices();
 
-        // tableau avec toutes les recettes et leur prix total
-        $recipesPrice = [];
-        foreach ($recipes as $recipe) {
-            $price = $em->getRecipieTotalPrice($recipe->getId()); 
-            //dump($price);
-            $recipesPrice[] = ['id' => $recipe->getId(), 'price' => intval($price[0]['totalPrice'])];
-        }
+        // on écrase le tableau précédent avec un tableau 
+        // contenant uniquement des recettes correspondant plus ou moins au prix objectif
+        $recipes = $this->getTargetedRecipesWithPrices($recipes, $targetPrice);
 
-        // tableau avec uniquement des recettes correspondant plus ou moins au prix objectif
-        $recipesTarget = [];
-
-        foreach ($recipesPrice as $recipe) {
-            $diff = intval($recipe['price']) - $targetPrice;
-
-            if ($diff <= 5 && $diff >= -5) {
-                $recipesTarget[] = $recipe;
-            }
-        }
-        $recipesTarget = array_column($recipesTarget, 'price', 'id');
-
-        // tableau menu avec les 21 recettes
-        $menu = [];
-        // tableau numérique temporaire ([numKey => recipeId]) pour pouvoir tirer au hasard
-        // une recette sur la base du nombre généré plus bas
-        $a = array_keys($recipesTarget);
-        dump('recipesTarget', $recipesTarget);
-        dump('tempArray', $a);
-
-        // built menu with random recipes
-        for ($i = 0; $i < $quantity; $i++) {
-            // génére un nb au hasard compris dans la range du nombre total de recettes
-            $n = rand(0, count($recipesTarget) - 1);
-            // stocke dans $key l'id de la recette générée au hasard
-            $key = $a[$n];
-            // stocke dans $value la valeur (prix total) de la recette générée au hasard
-            $value = $recipesTarget[$key];
-
-            if (!array_key_exists($key, $menu)) {
-                // construit le nouveau tableau menu si l'item n'est pas déjà présent
-                $menu[$key] = $value;
-            } else {
-                // sinon, relance la boucle au même stade
-                $i--;
-                continue;
-            }
-        }
-
-        // tableau avec les recettes non sélectionnées
-        $left = [];
-        foreach ($recipesTarget as $key => $value) {
-            if (!array_key_exists($key, $menu)) {
-                $left[$key] = $value;
-            }
-        }
-        dump('left', $left);
-        dump('menu', $menu);
+        $menus = $this->buildMenu($recipes, $quantity);
 
         // calcul du prix total du menu
-        $total = 0;
-        foreach ($menu as $m) {
-            $total += $m;
-        }
-        dump('total : ' . $total);
-        dump('budget : ' . $data['budget']);
+        $total = $this->getMenuTotalPrice($menus['menu']);
 
-        if ($total <= $data['budget']) {
-            dump('yeah');
-        } else {
-            // get id of most expensive recipe from $menu
-            // NB max et min retournent la valeur de l'élément trouvé, et non l'index
-            // il faut donc chercher ensuite avec array_search
-            $max = array_search(max($menu), $menu);
-            dump($max);
-            // get id of lowest expensive recipe from $recipeTarget
-            $min = array_search(min($left), $left);
-            dump($min);
+        $menu = $this->adjustMenu($menus['menu'], $menus['menuLeft'], $budget);
 
-            // enlever le plus chere
-            unset($menu[$max]);
-            // ajouter le moins cher
-            $menu[$min] = $recipesTarget[$min];
-            //array_splice($menu, $max, 1);
-            dump($menu);
-            //$min = 
-            //dump($prices);
-            //dump($max);
-            dump('nope');
-        }
+        return $this->json($menu);
         exit;
 
-
+        // LATER
         //$em = $this->getDoctrine()->getRepository(Recipe::class);
         //$totalPrice = $em->getRecipieTotalPrice(3);
         //dump($totalPrice);
@@ -175,8 +100,105 @@ class ObjectifController extends AbstractController
         //
     }
 
-    public function adjustMenu($menu)
+    public function adjustMenu($menu, $left, $budget)
     {
+        while ($this->getMenuTotalPrice($menu) > $budget) {
+            // NB max et min retournent la valeur de l'élément trouvé, et non l'index
+            // il faut donc chercher ensuite avec array_search pour avoir l'id de la recette
 
+            // get id of most expensive recipe from $menu
+            $max = array_search(max($menu), $menu);
+            dump('max supprimé : ', $menu[$max]);
+            // get id of lowest expensive recipe from $left
+            $min = array_search(min($left), $left);
+            dump('min ajouté : ', $left[$min]);
+
+            // enlever le plus chere du menu
+            unset($menu[$max]);
+            // ajouter le moins cher au menu
+            $menu[$min] = $left[$min];
+            // enlever le moins cher du $left pour 
+            // être certain de ne pas retomber dessus à la prochaine boucle
+            unset($left[$min]);
+        }
+
+        return $menu;
+    }
+
+    public function getMenuTotalPrice($menu)
+    {
+        $total = 0;
+        foreach ($menu as $m) {
+            $total += $m;
+        }
+        return $total;
+    }
+
+    public function getAllRecipesWithPrices()
+    {
+        $em = $this->getDoctrine()->getRepository(Recipe::class);
+        $recipes = $em->findAll();
+
+        // tableau avec toutes les recettes et leur prix total
+        $array = [];
+        foreach ($recipes as $recipe) {
+            $price = $em->getRecipieTotalPrice($recipe->getId()); 
+            $array[] = ['id' => $recipe->getId(), 'price' => intval($price[0]['totalPrice'])];
+        }
+        return $array;
+    }
+
+    public function getTargetedRecipesWithPrices($recipes, $targetPrice)
+    {
+        $newRecipes = [];
+        foreach ($recipes as $recipe) {
+            $diff = intval($recipe['price']) - $targetPrice;
+
+            if ($diff <= 5 && $diff >= -5) {
+                $newRecipes[] = $recipe;
+            }
+        }
+        $newRecipes = array_column($newRecipes, 'price', 'id');
+        return $newRecipes;
+    }
+
+    public function buildMenu($recipes, $quantity)
+    {
+        // tableau menu avec les 21 recettes sélectionnées
+        $menu = [];
+        // tableau avec les recettes non sélectionnées
+        $menuLeft = [];
+
+        // tableau numérique temporaire ([numKey => recipeId]) pour pouvoir tirer au hasard
+        // une recette sur la base du nombre généré plus bas
+        $array = array_keys($recipes);
+
+        // built menu with random recipes
+        for ($i = 0; $i < $quantity; $i++) {
+            // génére un nb au hasard compris dans la range du nombre total de recettes
+            $n = rand(0, count($recipes) - 1);
+            // stocke dans $key l'id de la recette générée au hasard
+            $key = $array[$n];
+            // stocke dans $value la valeur (prix total) de la recette générée au hasard
+            $value = $recipes[$key];
+
+            if (!array_key_exists($key, $menu)) {
+                // construit le nouveau tableau menu si l'item n'est pas déjà présent
+                $menu[$key] = $value;
+            } else {
+                // sinon, relance la boucle au même stade
+                $i--;
+                continue;
+            }
+        }
+
+        // built menu left for futur purposes
+        foreach ($recipes as $key => $value) {
+            if (!array_key_exists($key, $menu)) {
+                $menuLeft[$key] = $value;
+            }
+        }
+
+        return ['menu' => $menu, 'menuLeft' => $menuLeft];
     }
 }
