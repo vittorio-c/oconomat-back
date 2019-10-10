478cb316f304b514828f311b11bf1df9262ef310
Joffreycormont <j.cormont30@gmail.com>
2019-10-10 18:48:29 +0200

    adding method for forgotten pasword (sending email and persist the new encoded password in database)

--- oconomat/src/Controller/SecurityController.php
+++ oconomat/src/Controller/SecurityController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Swift_Mailer;
+use App\Service\getRandomPassword;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
@@ -15,7 +18,12 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
 
+    private $mailer;
 
+    public function __construct(\Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
     /**
      * @Route("/api/logout", name="app_logout")
      */
@@ -43,7 +51,6 @@ class SecurityController extends AbstractController
 
         $emailList = $this->getDoctrine()->getRepository(User::class)->getEmailList();
 
-        //dump($emailList);
 
         foreach($emailList as $emailArray){
             foreach($emailArray as $value){
@@ -72,8 +79,68 @@ class SecurityController extends AbstractController
             return $this->json('Les mots de passe ne correspondent pas !');
         }
 
-
-
-
     }
+
+
+    /**
+     * @Route("/api/password/new", name="app_password_new", methods={"POST"})
+     */
+    public function forgotPassword(Request $request, getRandomPassword $randomPassword, \Swift_Mailer $mailer, UserPasswordEncoderInterface $encoder)
+    {
+
+        // Vérifier l'utilisateur en cherchant son email dans la base de données
+        $email = $request->request->get('email');
+        $emailList = $this->getDoctrine()->getRepository(User::class)->getEmailList();
+
+        foreach($emailList as $emailArray){
+            foreach($emailArray as $value){
+                if($email == $value){
+
+                    // Trouver l'id utilisateur en fonction de l'email trouvée en bdd
+                    $userIdByEmail = $this->getDoctrine()->getRepository(User::class)->findUserIdByEmail($email); 
+                    $userId =  $userIdByEmail[0]['id'];
+
+                    $currentUser = $this->getDoctrine()->getRepository(User::class)->find($userId);
+
+                    // Créer un nouveau mot de passe
+
+                    $newPasswordGenerated = $randomPassword->generateRandomString();
+                    
+                    // L'envoyer via email de l'utilisateur 
+
+                    $message = (new \Swift_Message('Votre nouveau mot de passe'))
+                    ->setFrom($email)
+                    ->setTo($email)
+                    ->setBody(
+                        $this->renderView(
+                            // templates/emails/registration.html.twig
+                            'emails/newpassword.html.twig',
+                            ['password' => $newPasswordGenerated]
+                        ),
+                        'text/html'
+                    )
+                    ;
+            
+                    $mailer->send($message);
+
+                    // Encoder ce même mot de passe
+
+                    $encodedPassword = $encoder->encodePassword($currentUser, $newPasswordGenerated);
+                    $currentUser->setPassword($encodedPassword);
+
+                    // le persister dans la base de données
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($currentUser);
+                    $em->flush();
+
+                   
+                    return $this->json('Email avec votre nouveau mot de passe envoyé à'.$email);
+                }
+            }
+        }
+        
+    }
+
+
 }
